25e816a3c119325e824dc5b68a9417c3cc86e243
Gerberg1 <masi0001@stud.kea.dk>
2024-09-12 12:49:57 +0200

    Initial commit

---
+++ src/main/java/com/example/tekno1/Tekno1Application.java
@@ -0,0 +1,13 @@
+package com.example.tekno1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tekno1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tekno1Application.class, args);
+    }
+
+}
